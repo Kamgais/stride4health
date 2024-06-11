05cc5d02c9dd1bb83a4dca6e5c5003fece1d1ee3
Cyril Kamgais <102966559+Kamgais@users.noreply.github.com>
2024-06-11 11:05:21 +0200

    small modification

--- src/main/java/com/thb/mux/stride4health/repositories/ITrainingsDay.java
+++ src/main/java/com/thb/mux/stride4health/repositories/ITrainingsDay.java
@@ -5,7 +5,6 @@ import com.thb.mux.stride4health.entities.TrainingsDay;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 
-import java.util.UUID;
 
 @RepositoryDefinition(domainClass = TrainingsDay.class, idClass = Long.class)
 public interface ITrainingsDay extends CrudRepository<TrainingsDay,Long> {
